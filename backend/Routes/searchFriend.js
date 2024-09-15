e225160cf6bc2af8b1aa57caff22c88175ea5937
rahul-11105 <rahullokhande11105@gmail.com>
2024-09-15 11:50:41 +0530

    searchFriend route created

---
+++ backend/Routes/searchFriend.js
@@ -0,0 +1,25 @@
+const User = require("../model/user"); 
+const express = require("express");
+const routes = express.Router();
+
+routes.post('/', async (req, res) => {
+    try {
+        //console.log("Received request body:", req.body);
+        const user_name = req.body.Name;
+        // Find users 
+        //console.log(user_name);
+        const users = await User.find({Name:user_name});
+        // Check if users were found
+        if (users.length === 0) {
+            return res.status(404).send("No user exists with that name");
+        }
+
+        // Send the found users as JSON response
+        res.json(users);
+    } catch (err) {
+        console.error("Error fetching users:", err);
+        res.status(500).send("Server error");
+    }
+});
+
+module.exports = routes;
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const main = require("./connection/connectToDb");
 const user = require("./model/user");
+const searchFriend = require("./Routes/searchFriend");
 const port = process.env.PORT || 8000;
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
 
 
 //mongoDB connection
@@ -14,19 +20,20 @@ main("mongodb://127.0.0.1:27017/FriendNet").then((res)=>{
 });
 
 //routes
-app.get("/",async(req,res)=>{
-    const user1 = new user({
-        Name:"rahul",
-        Email:"rahullokhande@11105gmail.com"
-    });
-    try {
-        await user1.save();
-        res.send("<h1>Welcome to FriendNet</h1>");
-    } catch (err) {
-        res.status(500).send("Error saving user: " + err.message);
-    }
+app.use('/users',searchFriend);
+// app.get("/",async(req,res)=>{
+//     const user1 = new user({
+//         Name:"rahul",
+//         Email:"rahullokhande@11105gmail.com"
+//     });
+//     try {
+//         await user1.save();
+//         res.send("<h1>Welcome to FriendNet</h1>");
+//     } catch (err) {
+//         res.status(500).send("Error saving user: " + err.message);
+//     }
     
-});
+// });
 
 app.listen(port,()=>{
     console.log(`app is listening at port ${port}`);
