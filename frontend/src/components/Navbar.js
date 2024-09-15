f7c19645edb459e56885f451b81475c480fbc062
rahul-11105 <rahullokhande11105@gmail.com>
2024-09-15 08:59:07 +0530

    Navbar added

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../public/Navbar.css';
+
+export default function Navbar() {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="logo">
+        FriendNet
+      </div>
+    </nav>
+    </>
+  )
+}
