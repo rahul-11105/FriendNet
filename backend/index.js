d0dca590a91639284f347dbd1ab7ff3adaac3125
rahul-11105 <rahullokhande11105@gmail.com>
2024-09-15 10:00:05 +0530

    mongodb connected

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const main = require("./connection/connectToDb");
 const port = process.env.PORT || 8000;
 
 
-
+//mongoDB connection
+main("mongodb://127.0.0.1:27017/FriendNet").then((res)=>{
+    console.log("DB connected successfully");
+})
+.catch((err)=>{
+    console.log("mongoDB connection error",err);
+});
 
 //routes
 app.get("/",(req,res)=>{
